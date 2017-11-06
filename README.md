d6fc83d32a4d44ed66e87eb9ec8811b1215e8a6d
Jason Zhang <jason721b@gmail.com>
2017-11-05 20:53:12 -0800

    Add a new class Member to interact w/ Elasticsearch

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
 # Development
 
 ```sh
+# Install Python dependencies
 pip install -r requirements.txt
+
+# Install Elasticsearch
+brew install elasticsearch
+
+# Install Kibana, a visualization and debugging tool for Elasticsearch
+brew install kibana
+```
+
+# Launch Elasticsearch/Kibana
+
+```sh
+# You can replace "start" with "stop" or "restart"
+brew services start elasticsearch
+
+# Similarly, you can start Kibana using this
+brew services start kibana
 ```
 
 # Launch Web App
+
+Remember to launch Elasticsearch before launching the web app
 
 ```
 # Make sure the working directory is where `app.py` is located
 FLASK_APP=./app.py flask run
 ```
+
---
+++ members.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import DocType, Keyword
+from elasticsearch.helpers import scan
+
+
+# TODO: Hardcoded Elasticsearch client connecting to localhost instance
+es = Elasticsearch()
+
+
+class Member(DocType):
+
+    class Meta:
+        index = 'member'
+        doc_type = 'docs'
+        using = es
+
+    first_name = Keyword()
+    last_name = Keyword()
+
+    @classmethod
+    def all(cls):
+        """
+
+        Returns
+        -------
+        iterator<Member>
+
+        """
+
+        # NOTE: `scan` is usually not suitable for customer-facing web app
+        #   However, as this web app is for internal use and there are less than one thousand records
+        #   it should be OK to use
+        for member in scan(es, index='member', size=10):
+            yield Member(**member['_source'])
+
