0f8f7bb4f8eb163c856d52f8e5fb87dfdb04208e
Jason Zhang <jason721b@gmail.com>
2017-11-05 20:54:06 -0800

    Add routes and placeholder template

--- app.py
+++ app.py
@@ -1,7 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import logging
+
 from flask import Flask
+from flask import render_template
+from members import Member
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.INFO, stream=sys.stderr,
+                    format='%(asctime)s [%(name)s] %(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+
+# This creates an Elasticsearch index for Member model
+#    Elasticsearch index can be treated as table in SQL database
+Member.init()
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/random_group')
+def random_group():
+    return render_template('random_group.html')
+
+
+@app.route('/users/new')
+def new_user():
+    return render_template('new_user.html')
+
+
+@app.route('/users/create', methods=['POST'])
+def create_user():
+    return 'Create New Users'
+
+
+@app.route('/users/')
+def list_user():
+    return render_template('list_users.html', members=Member.all())
+
